63793f3e87422dd44474204a12f5d3ac4e8109f8
KROKODILCHIK234 <ekonstantine2610@mail.ru>
2024-04-14 21:42:10 +0800

    commit

--- Src/Logics/storage_service.py
+++ Src/Logics/storage_service.py
@@ -11,9 +11,13 @@ from Src.Storage.storage import storage
 from datetime import datetime
 import json
 
+from Src.settings import settings
+
 
 class storage_service:
     __data = []
+    __options = None
+    __blocked = []
 
     def __init__(self, data: list) -> None:
         if len(data) == 0:
@@ -41,7 +45,7 @@ class storage_service:
         turns = processing().process(data)
         return turns
 
-        # Набор основных методов
+    # Набор основных методов
 
     def create_turns(self, start_period: datetime, stop_period: datetime) -> list:
         """
@@ -68,7 +72,7 @@ class storage_service:
     def create_turns_by_nomenclature(self, start_period: datetime, stop_period: datetime,
                                      nomenclature: nomenclature_model) -> list:
         """
-            Получить обороты за период по конкретной номенклатуре
+            Получить обороты за период по конкретной номенклатуры
         Args:
             start_period (datetime): Начало
             stop_period (datetime): Окончание
@@ -86,10 +90,10 @@ class storage_service:
 
         # Фильтруем
         prototype = storage_prototype(self.__data)
-        filter = prototype.filter_by_period(stop_period, start_period)  # Reversed the start and stop period here.
+        filter = prototype.filter_by_period(start_period, stop_period)
         filter = filter.filter_by_nomenclature(nomenclature)
         if not filter.is_empty:
-            raise operation_exception(f"Невозможно сформировать обороты по указанным данным: {filter.error}")
+            raise operation_exception(f"Невозможно сформировать обороты по указанным данных: {filter.error}")
 
         return self.__processing(filter.data)
 
@@ -196,5 +200,71 @@ class storage_service:
             status=200,
             mimetype="application/json; charset=utf-8"
         )
+
+        return result
+
+    def create_blocked_turns(self):
+        pass
+
+    @property
+    def options(self):
+        return self.__options
+
+    @options.setter
+    def options(self, value: settings):
+        if not isinstance(value, settings):
+            raise argument_exception("неверный аргумент")
+        self.__options = value
+
+    # объединить обороты
+    @staticmethod
+    def _colide_turns(base_turns: list, added_turns: list):
+        if len(added_turns) == 0:
+            return base_turns
+
+        for index, cur_base_turn in enumerate(base_turns):
+
+            for aded_index, cur_added_turn in enumerate(added_turns):
+
+                if cur_base_turn.nomenclature == cur_added_turn.nomenclature and cur_base_turn.storage_id == cur_added_turn.storage_id:
+                    base_turns[index].amount += cur_added_turn.amount
+                    added_turns.pop(aded_index)
+                    break
+
+        for cur_added_turn in added_turns:
+            base_turns.append(cur_added_turn)
+        return base_turns
+
+    # получить обооты за период по номенклатуре
+    def create_turns_by_nomenclature(self, start_date: datetime, finish_date: datetime, id: uuid.UUID) -> dict:
+
+        if not isinstance(start_date, datetime) or not isinstance(finish_date, datetime):
+            raise argument_exception("Неверный аргумент")
+
+        if start_date > finish_date:
+            raise argument_exception("Неверно переданы аргументы")
+
+        prototype = storage_prototype(self.__data)
+
+        transactions = prototype.filter_number_id(id)
+
+        base = storage_prototype(self.__blocked).filter_number_id(id)
+
+        reference = reference_conventor(nomenclature_model,
+                                        error_proxy,
+                                        nomenclature_group_model,
+                                        range_model,
+                                        storage_journal_row,
+                                        storage_turn_model)
+
+        proces = process_factory()
+
+        data = proces.create(storage.process_turn_key(), transactions.data)
+
+        data = self._colide_turns(base.data, data)
+
+        result = {}
+        for index, cur_tran in enumerate(data):
+            result[index] = reference.convert(cur_tran)
 
         return result
--- Src/settings_manager.py
+++ Src/settings_manager.py
@@ -4,100 +4,95 @@ import uuid
 
 from Src.settings import settings
 from Src.errors import error_proxy
-from Src.exceptions import exception_proxy
 
 
-#
-# Менеджер настроек
-#   
 class settings_manager(object):
     # Наименование файла по умолчанию
-    _settings_file_name = "settings.json"
+    __settings_file_name = "settings.json"
     # Словарь с исходными данными
-    _data = None
+    __data = None
     # Внутренний уникальный номер
-    _uniqueNumber = None
+    __uniqueNumber = None
     # Данные с настройками
-    _settings = None
+    __settings = None
     # Описание ошибок
-    _error = error_proxy()
-    
+    __error = error_proxy()
+
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(settings_manager, cls).__new__(cls)
-        return cls.instance  
-      
+        return cls.instance
 
     def __init__(self):
-        if self._uniqueNumber is None:
-            self._uniqueNumber = uuid.uuid4()
-            self.open(self._settings_file_name)
-            
+        if self.__uniqueNumber is None:
+            self.__uniqueNumber = uuid.uuid4()
+            self.opener(self.__settings_file_name)
+
             # После загрузки создаем объект класса settings
-            self._settings = settings()
+            self.__settings = settings()
             self.__load()
-                
+
+    @property
+    def unique_number(self):
+        return self.__uniqueNumber
 
     def __open(self):
         """
             Открыть файл с настройками
         """
         file_path = os.path.split(__file__)
-        settings_file = "%s/%s" % (file_path[0], self._settings_file_name)
+        settings_file = "%s/%s" % (file_path[0], self.__settings_file_name)
         if not os.path.exists(settings_file):
-            self._error.set_error( Exception("ERROR: Невозможно загрузить настройки! Не найден файл %s", settings_file))
+            self.__error.set_error(Exception("ERROR: Невозможно загрузить настройки! Не найден файл %s", settings_file))
 
         try:
             with open(settings_file, "r") as read_file:
-                self._data = json.load(read_file)     
+                self.__data = json.load(read_file)
         except:
-            self._error.set_error( Exception("ERROR: Невозможно загрузить настройки! Не найден файл %s", settings_file))     
+            self.__error.set_error(Exception("ERROR: Невозможно загрузить настройки! Не найден файл %s", settings_file))
 
-    def open(self, file_name: str):
+    def opener(self, file_name: str):
         """
             Открыть файл с настройками
         Args:
             file_name (str):
         """
-        exception_proxy.validate( file_name, str)
-            
+        error_proxy(Exception(isinstance(file_name, str)))
+
         self._settings_file_name = file_name
         self.__open()
         self.__load()
-    
-    
+
     def __load(self):
         """
             Private: Загрузить словарь в объект
         """
-        
-        if len(self._data) == 0:
+
+        if len(self.__data) == 0:
             return
-        
-        # Список полей от типа назначения    
-        fields = list(filter(lambda x: not x.startswith("_"), dir(self._settings.__class__)))
-        
-        # Заполняем свойства 
+
+        # Список полей от типа назначения
+        fields = list(filter(lambda x: not x.startswith("_"), dir(self.__settings.__class__)))
+
+        # Заполняем свойства
         for field in fields:
-            keys = list(filter(lambda x: x == field, self._data.keys()))
+            keys = list(filter(lambda x: x == field, self.__data.keys()))
             if len(keys) != 0:
-                value = self._data[field]
-                
+                value = self.__data[field]
+
                 # Если обычное свойство - заполняем.
                 if not isinstance(value, list) and not isinstance(value, dict):
-                    setattr(self._settings, field, value)
-                
-        
-    
-    @property    
+                    setattr(self.__settings, field, value)
+
+    @property
     def settings(self) -> settings:
         """
             Текущие настройки в приложении
         Returns:
             settings: _
         """
-        return self._settings 
-    
+        return self.__settings
+
     @property
     def data(self):
         """
@@ -105,16 +100,13 @@ class settings_manager(object):
         Returns:
             dict:
         """
-        return self._data
-    
+        return self.__data
+
     @property
     def error(self) -> error_proxy:
         """
             Текущая информация об ошибке
         Returns:
-            error_proxy: 
+            error_proxy:
         """
-        return self._error
-
-
-    
+        return self.__error
